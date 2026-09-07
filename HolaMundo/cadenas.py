01b95cf83e00c92516b8f4bccff5946c76e4b1a8
jeissonj <pescres@gmail.com>
2026-09-07 03:51:14 -0500

    Se trabajo con las subcadenas [inicio: final +1]

--- HolaMundo/cadenas.py
+++ HolaMundo/cadenas.py
@@ -1,4 +1,11 @@
 # Manejo de cadenas
 
 mi_cadena = "\tUbaldo \nAcosta"
-print(mi_cadena)
+# print(mi_cadena)
+
+mensaje = "Hola Mundo"
+
+subcadena_hola = mensaje[0:4]
+subcadena_mundo = mensaje[5:]  # tambíen se podría con [5:10]
+
+print(subcadena_hola, "\n", subcadena_mundo)
