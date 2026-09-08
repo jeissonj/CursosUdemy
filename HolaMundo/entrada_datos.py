a2b33e592fac7b58c01bd4380008da6231a3b4df
jeissonj <pescres@gmail.com>
2026-09-08 14:53:36 -0500

    Se añadio la forma de ntrar daros por consola que es con input()

--- HolaMundo/entrada_datos.py
+++ HolaMundo/entrada_datos.py
@@ -1,1 +1,14 @@
 # Entrada de Datos por consola: input()
+#
+# mensaje = input("Proporcioan un mensaje")
+# print(f"El mensaje recibido es {mensaje}")
+#
+
+# int() -> convierte una cadena a un tipo entero
+numero1 = int(input("Cual es tu primer número?: "))
+print(numero1)
+numero2 = int(input("Cuál es tu segundo número?: "))
+print(numero2)
+
+resultado_suma = int(numero1) + int(numero2)
+print(f"El resltado de la suma es : {resultado_suma}")
