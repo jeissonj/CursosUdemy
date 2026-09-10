b89ef325347c1c121fcb400732a08b9416e6ec37
jeissonj <pescres@gmail.com>
2026-09-10 15:22:01 -0500

    Se añadieron programas de cajero automatico usando while e if

---
+++ operadores/cajero_automatico.py
@@ -0,0 +1,29 @@
+# programa para hacer el ejercicio del cajero automatico
+print("***  Cajero automarico de ciudad gotica ***")
+
+salir = True
+opcion = 0
+saldo = 10000
+modificacion = 0
+
+while salir:
+    print(f"""Operaciones que puedes realizar:
+        1. Consultar saldo
+        2. Retirar 
+        3. Depositar 
+        4. Salir 
+        Escoja una opción: """)
+    opcion = int(input())
+    if opcion == 1:
+        print(f"Su saldo es de {saldo}")
+    elif opcion == 2:
+        modificacion = int(input("Ingrese saldo a retirar: "))
+        saldo -= modificacion
+    elif opcion == 3:
+        modificacion = int(input("Ingrese saldo a depositar: "))
+        saldo += modificacion
+    elif opcion == 4:
+        print("Gracias por visitarnos hasta luego")
+        salir = False
+    else:
+        print("Error, Seleccione una de las opciones indicadas")
---
+++ operadores/ciclo_for.py
@@ -0,0 +1,9 @@
+print("*** Ciclo for ***")
+cadena = "Hola Mundo"
+
+for letra in cadena:
+    print(letra, end=" ")
+
+# Imprimir del 1 al 5
+for contador in range(1, 6):  # rango(iinicio, fin + 1)
+    print(contador)
