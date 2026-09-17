271e8147cb5789c6f2870f6aa66148b3d4b1b073
jeissonj <pescres@gmail.com>
2026-09-17 13:16:31 -0500

    Se cambio la manera de llamar el modulo de las funciones

--- global_mentoring_7_days/colecciones/maquina_snaks/maquina_snaks.py
+++ global_mentoring_7_days/colecciones/maquina_snaks/maquina_snaks.py
@@ -1,6 +1,5 @@
 # Maquina de snacks
-
-from modulo_maquina import mostrar_productos
+import modulo_maquina as mm
 
 # Primero vamos a crear unos productos y llaves iniciales en unas lisas que luego ingresaeremos a un diccionario y despues si desea el usuario podra modificar
 
@@ -10,4 +9,4 @@ llaves = ["ll1", "ll2", "ll3"]
 # La forma de unir los productos con las llaves es con la funcion zip
 l_productos = dict(zip(llaves, productos))
 
-mostrar_productos(l_productos)
+mm.mostrar_productos(l_productos)
