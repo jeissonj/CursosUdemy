c627a1c4a7a771ec51713cce442411f9a268e8c3
jeissonj <pescres@gmail.com>
2026-09-17 11:20:57 -0500

    manejamos el uso de modulos para guardar funciones y llamarlas desde otro archivo es como llamar nupy o matlab

---
+++ global_mentoring_7_days/colecciones/modulo_saludar.py
@@ -0,0 +1,6 @@
+# 1. Definir la funcion
+
+
+def saludar(parametro):
+    print(f"Mensaje recibido: {parametro}")
+    return "termina funcion ok!"
---
+++ global_mentoring_7_days/colecciones/uso_modulo_saludar.py
@@ -0,0 +1,8 @@
+from modulo_saludar import saludar
+
+print("*** Manejo de funciones ***")
+
+# 2. Llamamos a la funcion
+argumento = input("mennsaje a enviar: ")
+valor_devuelto = saludar(argumento)
+print(f"Valor devuelto de la funcion: {valor_devuelto}")
