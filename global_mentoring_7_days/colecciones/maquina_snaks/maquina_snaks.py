509a23e2a727bacf1d1fc5508e15729d4903daf0
jeissonj <pescres@gmail.com>
2026-09-19 22:59:43 -0500

    Se tienen los diccionarios que guradan los productos y las cantidades

--- global_mentoring_7_days/colecciones/maquina_snaks/maquina_snaks.py
+++ global_mentoring_7_days/colecciones/maquina_snaks/maquina_snaks.py
@@ -1,12 +1,13 @@
 # Maquina de snacks
 import modulo_maquina as mm
 
+print("*** Bien benido a la maquina expendedora ***")
 # Primero vamos a crear unos productos y llaves iniciales en unas lisas que luego ingresaeremos a un diccionario y despues si desea el usuario podra modificar
-
 productos = ["p1", "p2", "p3"]
 llaves = ["ll1", "ll2", "ll3"]
-
+precios = [1, 2, 3]
+lista = []
 # La forma de unir los productos con las llaves es con la funcion zip
 l_productos = dict(zip(llaves, productos))
-
-mm.mostrar_productos(l_productos)
+ordenar = True
+lista = mm.consulta(l_productos, lista)
--- global_mentoring_7_days/colecciones/maquina_snaks/modulo_maquina.py
+++ global_mentoring_7_days/colecciones/maquina_snaks/modulo_maquina.py
@@ -1,6 +1,27 @@
 # Modulo de maquina de esnaks
 # la idea es coloacr aca todas las funciones así de una vez ensayar lo de los modulos
+def consulta(l, lista):
+    pedir = input(
+        "Si desea ordenar un producto ingrese el numero 1, si no ingrese el 2: "
+    )
+    if pedir == "1":
+        lis = mostrar_productos(l, lista)
+    elif pedir == "2":
+        if lista != []:
+        print("La lista de los productos con sus precios es \n")
+        # y acá coloco la lista 
+                     
+        print("Para una proxima ")
+    else:
+        print("Opccion erronea ")
+        return lis
 
 
-def mostrar_productos(l):
-    print(l)
+def mostrar_productos(l, lista):
+    print(f"los productos disponibles son {list(l.keys())} ")
+    print(f"Y Los codigos para pedirlos son {list(l.values())} ")
+    lista.append(input("Ingrese el codigo del producto que desea pedir "))
+    return lista
+
+
+# def modifir_productp
