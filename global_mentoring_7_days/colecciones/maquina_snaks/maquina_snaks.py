030526960005dd78641fc32fea05129041f7e202
jeissonj <pescres@gmail.com>
2026-09-20 00:17:22 -0500

    Creo que ya ajjaj

--- global_mentoring_7_days/colecciones/maquina_snaks/maquina_snaks.py
+++ global_mentoring_7_days/colecciones/maquina_snaks/maquina_snaks.py
@@ -5,10 +5,13 @@ print("*** Bien benido a la maquina expendedora ***")
 # Primero vamos a crear unos productos y llaves iniciales en unas lisas que luego ingresaeremos a un diccionario y despues si desea el usuario podra modificar
 productos = ["p1", "p2", "p3"]
 llaves = ["ll1", "ll2", "ll3"]
-precios = [1, 2, 3]
-lista = {}
+pre = [1, 2, 3]
+dlista = {}
+valor = 0
 # La forma de unir los productos con las llaves es con la funcion zip
 l_productos = dict(zip(llaves, productos))
-lista = mm.llenar_lista(llaves, lista)
-ordenar = True
-lista = mm.consulta(l_productos, lista)
+precios = dict(zip(llaves, pre))
+# Acá hacemos una lista para las cantidades de los productos
+dlista = mm.llenar_lista(llaves, dlista)
+# Acá se tiene que preguntar si se va a comprar algo y diccionarlo
+mm.consulta(l_productos, dlista, precios)
--- global_mentoring_7_days/colecciones/maquina_snaks/modulo_maquina.py
+++ global_mentoring_7_days/colecciones/maquina_snaks/modulo_maquina.py
@@ -1,33 +1,50 @@
 # Modulo de maquina de esnaks
 # la idea es coloacr aca todas las funciones así de una vez ensayar lo de los modulos
-def llenar_lista(key,lista):
+def llenar_lista(key, dlista):
     for i in key:
-        lista[i] = 0
-    return lista
+        dlista[i] = 0
+    return dlista
 
 
-def consulta(l, lista):
-    pedir = input(
-        "Si desea ordenar un producto ingrese el numero 1, si no ingrese el 2: "
-    )
-    if pedir == "1":
-        lis = mostrar_productos(l, lista)
-    elif pedir == "2":
-        if lista != {}:
-        print("La lista de los productos con sus precios es \n")
-        # y acá coloco la lista 
-                     
-        print("Para una proxima ")
+def calculo_valor(dlista, precios):
+    mul = 0
+    for i in dlista.keys():
+        mul += dlista[i] * precios[i]
+    return mul
+
+
+def consulta(lp, dlista, precios):
+    ordenar = True
+    while ordenar:
+        pedir = input(
+            "Si desea ordenar un producto ingrese el numero 1, si no, ingrese el 2: "
+        )
+        if pedir == "1":
+            dlis = mostrar_productos(lp, dlista)
+        elif pedir == "2":
+            # Cómo es un diccionario necesitamos saber si la suma de los values es cero
+            valor = calculo_valor(dlista, precios)
+            if valor != 0:
+                print("La lista de los productos con sus precios es \n")
+                for i in list(lp.keys()):
+                    if dlista[i] != 0:
+                        print(f"{dlista[i]} {lp[i]} ... {precios[i]}")
+                print(f"El costo total de la transacción es {valor}")
+            print("Para una proxima ")
+            ordenar = False
+        else:
+            print("Opccion erronea ")
+
+
+def mostrar_productos(lp, dlista):
+    print(f"los productos disponibles son {list(lp.keys())} ")
+    print(f"Y Los codigos para pedirlos son {list(lp.values())} ")
+    x = input("Ingrese el codigo del producto que desea pedir ")
+    if x in lp.keys():
+        dlista[x] += 1
     else:
-        print("Opccion erronea ")
-        return lis
-
-
-def mostrar_productos(l, lista):
-    print(f"los productos disponibles son {list(l.keys())} ")
-    print(f"Y Los codigos para pedirlos son {list(l.values())} ")
-    lista.append(input("Ingrese el codigo del producto que desea pedir "))
-    return lista
+        print("El codigo ingresado es errroneo")
+    return dlista
 
 
 # def modifir_productp
